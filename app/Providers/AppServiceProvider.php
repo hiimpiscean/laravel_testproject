4dd6e7857446b0ba06434652ef936f2fb006b202
Piscean <conanchihieu@gmail.com>
2022-10-30 12:05:09 +0700

    Initial version

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        If (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+        //this is for future CSS stylesheets to be used with https connection from Heroku
     }
 }
